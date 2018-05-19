b760f50b6bd2b43de53107675bbfbbd283ca221d
DESKTOP-RQ7V5KF\Andrew <andrewmvalenz@gmail.com>
2018-05-18 22:10:12 -0700

    created window

--- OpenGL_Test_Environment/src/main.cpp
+++ OpenGL_Test_Environment/src/main.cpp
@@ -3,51 +3,69 @@
 
 
 #include <iostream>
-int main(void)
+
+/// constants
+const unsigned int scr_width = 800;
+const unsigned int scr_height = 600;
+
+/// function prototypes
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow *window);
+
+int main()
 {
-	GLFWwindow* window;
-
-	/* Initialize the library */
+	// initialize glfw
+	glfwInit();
 	if (!glfwInit())
-		return -1;
-
-
-	/* Create a windowed mode window and its OpenGL context */
-	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-	if (!window)
 	{
+		std::cout << "Failed to create initialize glfw" << std::endl;
+		return -1;
+	}
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	// create window and make current context
+	GLFWwindow* window = glfwCreateWindow(scr_width, scr_height, "OpenGL Test Window", NULL, NULL);
+
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
 		glfwTerminate();
 		return -1;
 	}
-
-	/* Make the window's context current */
 	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-
+	// initialize GLEW
 	if (glewInit() != GLEW_OK)
-		std::cout << "Error!" << std::endl;
+	{
+		std::cout << "Error! Failed to initialize GLEW" << std::endl;
+	}
 
-	std::cout << glGetString(GL_VERSION) << std::endl;
-
-	/* Loop until the user closes the window */
+	// rendering loop
 	while (!glfwWindowShouldClose(window))
 	{
-		/* Render here */
-		glClear(GL_COLOR_BUFFER_BIT);
-
-		glBegin(GL_TRIANGLES);
-		glVertex2f(-0.5f, -0.5f);
-		glVertex2f(-0.0f, 0.5f);
-		glVertex2f(0.5f, -0.5f);
-		glEnd();
-
-		/* Swap front and back buffers */
+		processInput(window);
+		
 		glfwSwapBuffers(window);
-
-		/* Poll for and process events */
 		glfwPollEvents();
 	}
 
 	glfwTerminate();
 	return 0;
+}
+
+// call whenever window size changes
+void framebuffer_size_callback(GLFWwindow * window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow * window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, true);
+	}
 }
