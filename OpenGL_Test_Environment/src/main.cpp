9c4e390f12fb2998b5df23d6aad7627feb7b1ba7
DESKTOP-RQ7V5KF\Andrew <andrewmvalenz@gmail.com>
2018-05-20 14:17:27 -0700

    Basic shaders and rendered single triangle

--- OpenGL_Test_Environment/src/main.cpp
+++ OpenGL_Test_Environment/src/main.cpp
@@ -4,13 +4,30 @@
 
 #include <iostream>
 
-/// constants
-const unsigned int scr_width = 800;
-const unsigned int scr_height = 600;
-
 /// function prototypes
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
+
+
+/// settings
+const unsigned int scr_width = 800;
+const unsigned int scr_height = 600;
+
+/// shaders
+const char *vertexShaderSrc = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+
+const char *fragShaderSrc = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"	FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"}\n\0";
+
 
 int main()
 {
@@ -43,11 +60,89 @@ int main()
 		std::cout << "Error! Failed to initialize GLEW" << std::endl;
 	}
 
+
+	/// build and compile shaders
+	unsigned int VertexShader, FragShader, ShaderProgram;
+	VertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(VertexShader, 1, &vertexShaderSrc, NULL);
+	glCompileShader(VertexShader);
+	// check for shader compile errors
+	int success;
+	char infoLog[512];
+	glGetShaderiv(VertexShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(VertexShader, 512, NULL, infoLog);
+		std::cout << "ERROR:: VertexShader compilation failed\n" << infoLog << std::endl;
+	}
+
+	FragShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(FragShader, 1, &fragShaderSrc, NULL);
+	glCompileShader(FragShader);
+	glGetShaderiv(FragShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(FragShader, 512, NULL, infoLog);
+		std::cout << "ERROR:: FragShader compilation failed\n" << infoLog << std::endl;
+	}
+
+	// link shaders
+	ShaderProgram = glCreateProgram();
+	glAttachShader(ShaderProgram, VertexShader);
+	glAttachShader(ShaderProgram, FragShader);
+	glLinkProgram(ShaderProgram);
+	glGetShaderiv(ShaderProgram, GL_LINK_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(ShaderProgram, 512, NULL, infoLog);
+		std::cout << "ERROR:: Shader linking failed\n" << infoLog << std::endl;
+	}
+	// clean up shaders
+	glDeleteShader(VertexShader);
+	glDeleteShader(FragShader);
+
+
+		
+	float vertices[] = {
+		-0.5f, -0.5f, 0.0f,
+		 0.5f, -0.5f, 0.0f,
+		 0.0f,  0.5f, 0.0f
+	};
+
+	// setup vao, vbo
+	unsigned int vao, vbo;
+	glGenVertexArrays(1, &vao);
+	glGenBuffers(1, &vbo);
+
+
+	glBindVertexArray(vao);
+
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	// unbind vertex array to prevent changes
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+
 	// rendering loop
 	while (!glfwWindowShouldClose(window))
 	{
 		processInput(window);
 		
+		// render here
+		glClearColor(.2f, .2f, .2f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glUseProgram(ShaderProgram);
+		glBindVertexArray(vao);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		
+
+		// end rendering
+
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
