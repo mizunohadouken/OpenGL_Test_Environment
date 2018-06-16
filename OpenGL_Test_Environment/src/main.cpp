cc82c0074037998e44d29a0c3b1eb8b97a948990
DESKTOP-RQ7V5KF\Andrew <andrewmvalenz@gmail.com>
2018-06-15 21:01:59 -0700

    Coordinate Systems, Rendered multiple boxes

--- OpenGL_Test_Environment/src/main.cpp
+++ OpenGL_Test_Environment/src/main.cpp
@@ -113,6 +113,18 @@ int main()
 		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f
 	};
 
+	glm::vec3 cubePositions[] = {
+		glm::vec3(0.0f,  0.0f,  0.0f),
+		glm::vec3(2.0f,  5.0f, -15.0f),
+		glm::vec3(-1.5f, -2.2f, -2.5f),
+		glm::vec3(-3.8f, -2.0f, -12.3f),
+		glm::vec3(2.4f, -0.4f, -3.5f),
+		glm::vec3(-1.7f,  3.0f, -7.5f),
+		glm::vec3(1.3f, -2.0f, -2.5f),
+		glm::vec3(1.5f,  2.0f, -2.5f),
+		glm::vec3(1.5f,  0.2f, -1.5f),
+		glm::vec3(-1.3f,  1.0f, -1.5f)
+	};
 
 	/*
 	unsigned int indices[] = {
@@ -242,21 +254,26 @@ int main()
 		int mixerLocation = glGetUniformLocation(shaderClass.ID, "mixer");
 		glUniform1f(mixerLocation, mixerVal);
 
+		glBindVertexArray(vao);
 		// set up MVP
-		glm::mat4 model = glm::mat4(1.0f);
-		model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
 		glm::mat4 view = glm::mat4(1.0f);
 		view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
 		glm::mat4 projection = glm::mat4(1.0f);
 		projection = glm::perspective(glm::radians(45.0f), (float)scr_width / (float)scr_height, 0.1f, 100.0f);
-		glm::mat4 mvp = projection * view * model;
 
 		unsigned int transformLoc = glGetUniformLocation(shaderClass.ID, "mvpMat");
-		glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(mvp));
 
-		glBindVertexArray(vao);
+		for (unsigned int i = 0; i < 10; i++)
+		{
+			glm::mat4 model = glm::mat4(1.0f);
+			model = glm::translate(model, cubePositions[i]);
+			model = glm::rotate(model, glm::radians(20.0f * i) , glm::vec3(1.0f, 0.3f, 0.5f));
+			glm::mat4 mvp = projection * view * model;
+			glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(mvp));
+
+			glDrawArrays(GL_TRIANGLES, 0, 36);
+		}
 	//	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-		glDrawArrays(GL_TRIANGLES, 0, 36);
 
 		// end rendering
 		glfwSwapBuffers(window);
